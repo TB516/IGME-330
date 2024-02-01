144a789eaf11f03493aa1f51a4f3d0aae0e18967
Thomas <tberrios9800@gmail.com>
2024-02-01 15:21:49 -0500

    Started HW 1 and finished xhr helper functions

---
+++ berrios-t-hw1/src/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const getRandomNum = (min = 0, max) => {
+    return Math.random() * (max - min) + min;
+}
+
+const babble = () => {
+    output.innerHTML = `${words1[Math.round(getRandomNum(0, words1.length - 1))]} ${words2[Math.round(getRandomNum(0, words2.length - 1))]} ${words3[Math.round(getRandomNum(0, words3.length - 1))]}`;
+}
+
+const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+
+const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+
+const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+
+window.onload = () => {
+    const button = document.querySelector("#myButton");
+    const output = document.querySelector("#output");
+
+    button.addEventListener("click", babble);
+
+    babble();
+}
