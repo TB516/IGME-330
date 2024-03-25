2ad2fe4ca8d178385ad50b4070da928dbc3d8168
Thomas <tberrios9800@gmail.com>
2024-03-25 13:36:13 -0400

    Fixed intro to node joke 1

--- berrios-t-node-intro/joke/index.js
+++ berrios-t-node-intro/joke/index.js
@@ -1,6 +1,5 @@
 // #1 - import the request module, which is used to download data over http
 const request = require('request');
-const cowsay = require("cowsay");
 
 // #2 - set our URL
 let url = 'https://geek-jokes.sameerkumar.website/api';
@@ -12,9 +11,6 @@ request(url, (err, response, body) => {
     // if there's no error, and if the server's status code is 200 (i.e. "Ok")
     if(!err && response.statusCode == 200){
     	// log out the plain-text joke - no parsing required!
-        console.log(cowsay.say({
-            text : body,
-            e : "oO"
-        }));
+        console.log(body);
     }
 });
